19f20df4c49ab0c3a98dc6017a64dd0f887af094
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2023-04-14 10:23:44 -0400

    fix edit profile bug

--- Models/LabUser.cs
+++ Models/LabUser.cs
@@ -9,11 +9,15 @@ public class LabUser
 	public string Email { get; set; } = string.Empty;
 
 	[Display(Name = "First Name")]
+
 	public string FirstName { get; set; } = string.Empty;
+	[DataType(DataType.Password)]
 	public string Password { get; set; } = string.Empty;
 
 	[NotMapped]
 	[Compare("Password", ErrorMessage = "Passwords do not match")]
+	[Display(Name = "Verify Password")]
+	[DataType(DataType.Password)]
 	public string PasswordVerify { get; set; } = string.Empty;
 
 }
--- Pages/Profile/Edit.cshtml.cs
+++ Pages/Profile/Edit.cshtml.cs
@@ -2,75 +2,87 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab3.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Lab3.Models;
 
 namespace lab3.Pages_Profile
 {
-    public class EditModel : PageModel
-    {
-        private readonly Lab3.Models.Lab3Context _context;
+	public class EditModel : PageModel
+	{
+		private readonly Lab3.Models.Lab3Context _context;
+		private readonly ILogger<EditModel> _logger;
 
-        public EditModel(Lab3.Models.Lab3Context context)
-        {
-            _context = context;
-        }
+		public EditModel(Lab3.Models.Lab3Context context, ILogger<EditModel> logger)
+		{
+			_context = context;
+			_logger = logger;
+		}
 
-        [BindProperty]
-        public LabUser LabUser { get; set; } = default!;
+		[BindProperty]
+		public LabUser LabUser { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(uint? id)
-        {
-            if (id == null || _context.Users == null)
-            {
-                return NotFound();
-            }
+		public async Task<IActionResult> OnGetAsync(uint? id)
+		{
+			if (id == null || _context.Users == null)
+			{
+				return NotFound();
+			}
 
-            var labuser =  await _context.Users.FirstOrDefaultAsync(m => m.LabUserId == id);
-            if (labuser == null)
-            {
-                return NotFound();
-            }
-            LabUser = labuser;
-            return Page();
-        }
+			var labuser = await _context.Users.FirstOrDefaultAsync(m => m.LabUserId == id);
+			if (labuser == null)
+			{
+				return NotFound();
+			}
+			LabUser = labuser;
+			return Page();
+		}
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see https://aka.ms/RazorPagesCRUD.
+		public async Task<IActionResult> OnPostAsync()
+		{
+			_logger.LogInformation("OnPostAsync");
+			if (!ModelState.IsValid)
+			{
+				_logger.LogInformation("invalid modelstate");
+				foreach (var ms in ModelState.Values)
+				{
+					foreach (var error in ms.Errors)
+					{
+						_logger.LogError(error.ErrorMessage);
+					}
+				}
 
-            _context.Attach(LabUser).State = EntityState.Modified;
+				return Page();
+			}
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!LabUserExists(LabUser.LabUserId))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			_context.Attach(LabUser).State = EntityState.Modified;
 
-            return RedirectToPage("./Index");
-        }
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!LabUserExists(LabUser.LabUserId))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-        private bool LabUserExists(uint id)
-        {
-          return (_context.Users?.Any(e => e.LabUserId == id)).GetValueOrDefault();
-        }
-    }
+			return RedirectToPage("./Details");
+		}
+
+		private bool LabUserExists(uint id)
+		{
+			return (_context.Users?.Any(e => e.LabUserId == id)).GetValueOrDefault();
+		}
+	}
 }
